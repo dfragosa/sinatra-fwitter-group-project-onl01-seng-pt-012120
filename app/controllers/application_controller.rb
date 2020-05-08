b8a0bd0224baa5fcdceb2397201d54d5c61acfd5
Derric Fragosa <fderric@gmail.com>
2020-05-08 13:34:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,14 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    erb :homepage
+  end
+
+  get '/signup'
+    erb :signup
+  end
+  
 
 end
